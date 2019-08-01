1577650b13be80950cdfd95a0e20676064e3b605
MiPiNi <YouTubePortoPL@wp.pl>
2019-08-01 18:53:22 +0200

    Fall damage
    Added fall damage script

---
+++ Assets/Gracz/Skrypty/FallDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDamage : MonoBehaviour
+{
+    public CharacterController controller;
+    public Stats stats;
+    public float minSurviveTime = 1.6f;
+    public float damageForSeconds = 1.5f;
+    public float airTime = 0f;
+
+
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!controller.isGrounded)
+        {
+            airTime += Time.deltaTime;
+        }
+        if (controller.isGrounded)
+        {
+            if (airTime > minSurviveTime)
+            {
+                stats.Health -= damageForSeconds * airTime;
+            }
+            airTime = 0f;
+        }
+    }
+}
