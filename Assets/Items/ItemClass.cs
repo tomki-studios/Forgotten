69f3008b69900c17f138dc56eae95cb40fba3978
ziomaliss123 <barteklisssss@gmail.com>
2019-08-01 16:18:39 +0200

    Początek klas itemów

---
+++ Assets/Items/ItemClass.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Items
+{
+    public class ItemClass : MonoBehaviour
+    {
+        public int eqPosition;
+        public float amount;
+        public float weight;
+        public float value;
+
+        public ItemClass(int eqPosition, float amount, float weight, float value)
+        {
+            eqPosition = this.eqPosition;
+            amount = this.amount;
+            weight = this.weight;
+            value = this.value;
+        }
+    }
+}
---
+++ Assets/Items/ItemTypes/WeaponTypes/RangeWeapon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Items.ItemTypes.WeaponTypes
+{
+    public enum projectileType
+    {
+        arrow, bullet, laser 
+    }
+
+    public class RangeWeapon : WeaponClass
+    {
+        public projectileType projType;
+        public int magazineSize;
+
+       
+
+        
+        public RangeWeapon( 
+            int eqPosition, 
+            float amount, 
+            float weight, 
+            float value, 
+            float damageOnHit, 
+            float usage, 
+            float attackSpeed, 
+            projectileType projType, 
+            int magazineSize
+        ) : base(eqPosition, amount, weight, value, damageOnHit, usage, attackSpeed)
+
+        {
+            projType = this.projType;
+            magazineSize = this.magazineSize;
+        }
+
+    }
+}
