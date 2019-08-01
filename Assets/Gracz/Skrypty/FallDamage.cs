27d4a5f54c2893e713ff2d91ca013505483bf64f
MiPiNi <YouTubePortoPL@wp.pl>
2019-08-01 23:28:59 +0200

    Updated FallDamage.cs
    Added comments to FallDamage script

--- Assets/Gracz/Skrypty/FallDamage.cs
+++ Assets/Gracz/Skrypty/FallDamage.cs
@@ -6,9 +6,9 @@ public class FallDamage : MonoBehaviour
 {
     public CharacterController controller;
     public Stats stats;
-    public float minSurviveTime = 1.6f;
-    public float damageForSeconds = 1.5f;
-    public float airTime = 0f;
+    public float minSurviveTime = 1.6f; //time you can be in air without dmg
+    public float damageForSeconds = 1.5f; //damage you will get every second after minSurviveTime
+    public float airTime = 0f; //actual time in air
 
 
     void Start()
@@ -18,17 +18,19 @@ public class FallDamage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!controller.isGrounded)
+        if (!controller.isGrounded) //when player is in air
         {
-            airTime += Time.deltaTime;
+            airTime += Time.deltaTime; //setting airTime
         }
-        if (controller.isGrounded)
+        if (controller.isGrounded) //when player isn't in air
         {
-            if (airTime > minSurviveTime)
+            if (airTime > minSurviveTime)  //when you pass minSurviveTime
             {
-                stats.Health -= damageForSeconds * airTime;
+                stats.Health -= damageForSeconds * airTime; //adding dmg to player
             }
-            airTime = 0f;
+            airTime = 0f;               //reset airTime
         }
     }
 }
+            //Script by Daniel Grala
+            
