9f5814c1feb215d3e4e2ac416059bfc904192ea2
Daniel Grala <YouTubePortoPL@wp.pl>
2019-08-02 17:56:35 +0200

    Beggining of Equipment system
    Added Equipment.cs
    Added MeleeClass.cs

---
+++ Assets/Items/ItemTypes/WeaponTypes/MeleeClass.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeClass : MonoBehaviour  //contains variables for melee weapons - check Equipment.Sword()
+{
+    public int dmg;
+    public int weight;
+    public int value;
+    public int attackSpeed;
+    public string description;
+}
+            //Script by Daniel Grala
