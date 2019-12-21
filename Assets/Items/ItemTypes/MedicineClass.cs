606ba25787531aab916d1c6250edfd303cccddf1
XCreckerX <33941168+XCreckerX@users.noreply.github.com>
2019-12-21 16:14:21 +0100

    MedicineItem class

---
+++ Assets/Items/ItemTypes/MedicineClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Items.ItemTypes
+{
+    public class MedicineClass : MonoBehaviour
+    {
+        public string MedicineName;
+        //Nazwa lekarstwa
+        public float MedicineRegen;
+        //Ilość HP które leczy lekarstwo
+        public float Weight;
+        //Waga
+
+    }
+}
